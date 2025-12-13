0dbe2cf798bcdcf7b7277f35f454df3437cf9de7
BONTHU RAMA SATYA TEJA <tejbonthu45@gmail.com>
2025-12-13 14:03:17 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* Add viewport meta tag to prevent auto-zooming */}
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        
         {/* Preconnect to external domains for faster loading */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
